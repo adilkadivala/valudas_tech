3096a91db4067947dee90446c82d7e75f9d73c72
k-adi <adilkadivala560@gmail.com>
2024-06-17 17:12:45 +0530

    some changes

--- client/src/pages/public/Hero.js
+++ client/src/pages/public/Hero.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../../assets/css/public/Hero.css";
+import { useValudasData } from "../../context/Storage";
 
 function Hero() {
-  const sliderImgs = [
-    { img: require("../../assets/images/awc.png") },
-    { img: require("../../assets/images/forolly 1.png") },
-    { img: require("../../assets/images/mumezshop.png") },
-    { img: require("../../assets/images/microface.png") },
-    { img: require("../../assets/images/awc.png") },
-    { img: require("../../assets/images/forolly 1.png") },
-    { img: require("../../assets/images/nandolia.png") },
-  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(true);
+  const slideTrackRef = useRef(null);
+  const { slider } = useValudasData();
+
+  useEffect(() => {
+    const autoSlide = () => {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    };
+
+    const intervalId = setInterval(autoSlide, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [slider.length]);
+
+  useEffect(() => {
+    const slideTrack = slideTrackRef.current;
+    if (!slideTrack) return;
+
+    const totalSlides = slider.length;
+    const firstSlide = slideTrack.children[0];
+    if (!firstSlide) return;
+
+    const slideWidth = firstSlide.offsetWidth;
+
+    if (currentIndex === totalSlides) {
+      slideTrack.style.transition = "none";
+      slideTrack.style.transform = `translateX(0)`;
+      setTimeout(() => {
+        setIsTransitioning(true);
+        setCurrentIndex(1);
+      }, -1);
+    } else {
+      slideTrack.style.transition = isTransitioning
+        ? "transform 0.5s linear"
+        : "none";
+      slideTrack.style.transform = `translateX(${
+        -currentIndex * slideWidth
+      }px)`;
+    }
+  }, [currentIndex, isTransitioning, slider.length]);
 
   return (
     <div className="hero">
@@ -30,13 +63,13 @@ function Hero() {
 
       <div className="hero_second">
         <div className="slider">
-          <div className="slide-track">
-            {sliderImgs.concat(sliderImgs).map((slide, index) => (
+          <div className="slide-track" ref={slideTrackRef}>
+            {slider.concat(slider).map((slide, index) => (
               <div className="slide" key={index}>
                 <img
-                  src={slide.img}
+                  src={`/upload/${slide.image}`}
                   alt="Testimonial user"
-                  style={{ width: "50%" }}
+                  style={{ width: "70%" }}
                 />
               </div>
             ))}
